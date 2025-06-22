08ecd9533db288e8c06d4086137cdae5e7be9aef
viral-glitch <viraldeaura@gmail.com>
2025-06-22 16:16:37 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,15 +3,15 @@ const fs = require('fs');
 const chalk = require('chalk');
 
 // Contact details
-global.ownernomer = process.env.OWNER_NOMER || "255672562733";
+global.ownernomer = process.env.OWNER_NOMER || "255742839217";
 global.ytname = process.env.YT_NAME || "YT: @EliteProTech";
 global.socialm = process.env.SOCIAL_M || "GitHub: EliteProTech";
 global.location = process.env.LOCATION || "Nigeria, Port Harcourt";
 
 // Creator details
-global.ownernumber = process.env.OWNER_NUMBER || '255672562733';
+global.ownernumber = process.env.OWNER_NUMBER || '255742839217';
 global.ownername = process.env.OWNER_NAME || 'Vai';
-global.botname = process.env.BOT_NAME || 'Viral';
+global.botname = process.env.BOT_NAME || 'Milton';
 
 // Default settings 
 global.prefa = process.env.PREFIX ? process.env.PREFIX.split(',') : ['', '!', '.', '#', '&'];
