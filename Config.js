7bea6121ce87921dfcdd0bdc5b55cc1efa5b0977
Flavie Kshlerin <viraldeaura@gmail.com>
2025-07-04 11:51:10 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,15 +3,15 @@ const fs = require('fs');
 const chalk = require('chalk');
 
 // Contact details
-global.ownernomer = process.env.OWNER_NOMER || "255692487412";
+global.ownernomer = process.env.OWNER_NOMER || "255672562733";
 global.ytname = process.env.YT_NAME || "YT: @EliteProTech";
 global.socialm = process.env.SOCIAL_M || "GitHub: EliteProTech";
 global.location = process.env.LOCATION || "Nigeria, Port Harcourt";
 
 // Creator details
-global.ownernumber = process.env.OWNER_NUMBER || '255692487412';
+global.ownernumber = process.env.OWNER_NUMBER || '255672562733';
 global.ownername = process.env.OWNER_NAME || 'Vai';
-global.botname = process.env.BOT_NAME || 'philip';
+global.botname = process.env.BOT_NAME || 'E';
 
 // Default settings 
 global.prefa = process.env.PREFIX ? process.env.PREFIX.split(',') : ['', '!', '.', '#', '&'];
