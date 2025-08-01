4864f04c9126f1303822e70aa57fb757b165256b
Flavie Kshlerin <viraldeaura@gmail.com>
2025-08-01 20:38:28 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const chalk = require('chalk');
 
 // Contact details
-global.ownernomer = process.env.OWNER_NOMER || "258865797980";
+global.ownernomer = process.env.OWNER_NOMER || "258845968682";
 global.ytname = process.env.YT_NAME || "YT: @EliteProTech";
 global.socialm = process.env.SOCIAL_M || "GitHub: EliteProTech";
 global.location = process.env.LOCATION || "Nigeria, Port Harcourt";
@@ -11,7 +11,7 @@ global.location = process.env.LOCATION || "Nigeria, Port Harcourt";
 // Creator details
 global.ownernumber = process.env.OWNER_NUMBER || '258865797980';
 global.ownername = process.env.OWNER_NAME || 'Vai';
-global.botname = process.env.BOT_NAME || 'Ton';
+global.botname = process.env.BOT_NAME || 'Kims';
 
 // Default settings 
 global.prefa = process.env.PREFIX ? process.env.PREFIX.split(',') : ['', '!', '.', '#', '&'];
@@ -20,7 +20,7 @@ global.autoRecording = process.env.AUTO_RECORDING === 'false';
 global.autoTyping = process.env.AUTO_TYPING === 'false';
 global.autorecordtype = process.env.AUTO_RECORD_TYPE === 'false';
 global.autoread = process.env.AUTO_READ === 'false';
-global.autobio = process.env.AUTO_BIO !== 'false'; // default true
+global.autobio = process.env.AUTO_BIO !== 'false'; // default false
 global.anti92 = process.env.ANTI_92 === 'true';
 global.autoswview = process.env.AUTO_SW_VIEW !== 'false'; // default true
 global.welcome = process.env.WELCOME !== 'false'; // default true
